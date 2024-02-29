6626952c05e45008f8e52c121cc496a4f3cc3ef5
SergeiRogov <rogov120301@gmail.com>
2024-02-29 11:26:56 +0200

    Introduced Formik

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -9,76 +9,97 @@ import {
   Text,
   useColorModeValue,
 } from "@chakra-ui/react";
+import { useFormik } from "formik";
 import { Link as ReactRouterLink } from "react-router-dom";
 
 export function Login() {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
-    <Flex
-      flexDirection="column"
-      justifyContent="center" // Center vertically
-      alignItems="center" // Center horizontally
-      height="100vh" // Set the height to the full viewport height
-    >
-      <Text
-        mb={4}
-        fontWeight="extrabold"
-        fontSize="25"
-        color={useColorModeValue("primary.light", "primary.dark")}
+    <form onSubmit={formik.handleSubmit}>
+      <Flex
+        flexDirection="column"
+        justifyContent="center" // Center vertically
+        alignItems="center" // Center horizontally
+        height="100vh" // Set the height to the full viewport height
       >
-        Login
-      </Text>
+        <Text
+          mb={4}
+          fontWeight="extrabold"
+          fontSize="25"
+          color={useColorModeValue("primary.light", "primary.dark")}
+        >
+          Login
+        </Text>
 
-      <Flex alignItems="center">
-        <InputGroup width="250px">
-          <InputLeftElement pointerEvents="none">
-            <AtSignIcon
-              color={useColorModeValue("primary.light", "primary.dark")}
+        <Flex alignItems="center">
+          <InputGroup width="250px">
+            <InputLeftElement pointerEvents="none">
+              <AtSignIcon
+                color={useColorModeValue("primary.light", "primary.dark")}
+              />
+            </InputLeftElement>
+            <Input
+              name="username"
+              placeholder="username"
+              width="100%" // Use 100% width for the Input
+              mb={4}
+              minHeight="40px"
+              onChange={formik.handleChange}
+              value={formik.values.username}
             />
-          </InputLeftElement>
-          <Input
-            type="username"
-            placeholder="username"
-            width="100%" // Use 100% width for the Input
-            mb={4}
-            minHeight="40px"
-          />
-        </InputGroup>
-      </Flex>
+          </InputGroup>
+        </Flex>
 
-      <Flex alignItems="center">
-        <InputGroup width="250px">
-          <InputLeftElement pointerEvents="none">
-            <UnlockIcon
-              color={useColorModeValue("primary.light", "primary.dark")}
+        <Flex alignItems="center">
+          <InputGroup width="250px">
+            <InputLeftElement pointerEvents="none">
+              <UnlockIcon
+                color={useColorModeValue("primary.light", "primary.dark")}
+              />
+            </InputLeftElement>
+            <Input
+              type="password"
+              name="password"
+              placeholder="password"
+              width="250px" // Set the width of the input field
+              mb={4} // Adds margin-bottom for spacing
+              minHeight="40px"
+              onChange={formik.handleChange}
+              value={formik.values.password}
             />
-          </InputLeftElement>
-          <Input
-            placeholder="password"
-            width="250px" // Set the width of the input field
-            mb={4} // Adds margin-bottom for spacing
-            minHeight="40px"
-          />
-        </InputGroup>
+          </InputGroup>
+        </Flex>
+
+        {/* <ReactRouterLink to="/app/courses"> */}
+        <Button variant="primary" type="submit">
+          Sign in
+        </Button>
+        {/* </ReactRouterLink> */}
+
+        <ChakraLink
+          as={ReactRouterLink}
+          to="/forgot-password"
+          mb={4}
+          fontSize="15"
+        >
+          forgot password
+        </ChakraLink>
+
+        <Text mb={4}>or</Text>
+
+        <ReactRouterLink to="/registration">
+          <Button variant="primary">Create new account</Button>
+        </ReactRouterLink>
       </Flex>
-
-      <ReactRouterLink to="/app/courses">
-        <Button variant="primary">Sign in</Button>
-      </ReactRouterLink>
-
-      <ChakraLink
-        as={ReactRouterLink}
-        to="/forgot-password"
-        mb={4}
-        fontSize="15"
-      >
-        forgot password
-      </ChakraLink>
-
-      <Text mb={4}>or</Text>
-
-      <ReactRouterLink to="/registration">
-        <Button variant="primary">Create new account</Button>
-      </ReactRouterLink>
-    </Flex>
+    </form>
   );
 }
